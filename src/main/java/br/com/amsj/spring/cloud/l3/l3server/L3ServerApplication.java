ed6c4afa2bb1948dc766d62491475f16cfa5717f
Ailton <ailton.msj@gmail.com>
2018-11-06 14:14:56 -0200

    initial commit

---
+++ src/main/java/br/com/amsj/spring/cloud/l3/l3server/L3ServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.amsj.spring.cloud.l3.l3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L3ServerApplication.class, args);
+	}
+}
